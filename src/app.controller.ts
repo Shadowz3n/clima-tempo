ef286a7a6b61a335393d0bf0af99c33f1c577f0e
Henrique Bissoli Silva <emp.shad@gmail.com>
2022-05-11 23:06:21 -0300

    fix: get hello response

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @Version('v1')
+  @Version('1')
   getHello(): string {
     return this.appService.getHello();
   }
