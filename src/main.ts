76b0a633c66fa01aaa919515f16e27d9849f5274
Henrique Bissoli Silva <emp.shad@gmail.com>
2022-05-11 23:05:33 -0300

    feat: Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { VersioningType } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Clima API')
+    .setDescription('API de previsão do clima')
+    .setVersion('1.0')
+    .addTag('clima')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
